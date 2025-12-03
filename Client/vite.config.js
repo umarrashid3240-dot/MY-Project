795a0d07fb6ea21390b2997e1f0d8b86baa8fcc7
umarrashid <umarrashid@gmail.com>
2025-12-03 20:00:04 +0500

    Set base path for GitHub Pages

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -17,8 +17,18 @@
 // })
 
 
+// import { defineConfig } from 'vite'
+
+// export default defineConfig({
+//   base: '/MY-Project/',   // repo کا نام
+// })
+
+
+// Client/vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/MY-Project/',   // repo کا نام
+  plugins: [react()],
+  base: '/MY-Project/',   // <-- exactly tumhare repo name
 })
